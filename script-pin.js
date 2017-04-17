9c876da19b729916d0162bea498a3fcf3c3e24ff
Kevin <keving17@mit.edu>
2017-04-17 00:12:13 -0400

    done pin and CSS

--- script-pin.js
+++ script-pin.js
@@ -1,5 +1,5 @@
 var FILTERS = ['grass', 'water', 'building', 'sculpture'];
-var NUM_PHOTOS = 4
+var NUM_PHOTOS = 3
 
 $(document).ready(function() {
 	for (p=0; p<NUM_PHOTOS; p++) {
@@ -13,29 +13,27 @@ $(document).ready(function() {
 			filters.push(f);
 		}
 		var row = document.createElement('div');
-		row.setAttribute('class','row');
+		row.setAttribute('class','row pin');
 		row.setAttribute('id','pinned');
 
 		var leftDiv = document.createElement('div');
-		leftDiv.setAttribute('class','col-md-3');
-		leftDiv.setAttribute('id','pin-col-photos');
+		leftDiv.setAttribute('class','col-md-3 pin-col-photos');
 		
 		var rightDiv = document.createElement('div');
-		rightDiv.setAttribute('class','col-md-9');
-		rightDiv.setAttribute('id','pin-col-photos');
+		rightDiv.setAttribute('class','col-md-9 pin-col-photos description');
 
 		row.appendChild(leftDiv);
 		row.appendChild(rightDiv);
 
 		var photo = document.createElement('div');
 		photo.setAttribute('id', 'photo-'+p);
-		photo.setAttribute('class', 'photo');
+		photo.setAttribute('class', 'photo pin');
 
 		var button = document.createElement('div');
 		button.setAttribute('class', 'heart-div');
 		var heart = document.createElement('input');
 		heart.setAttribute('type', 'image');
-		heart.setAttribute('src', 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f1/Heart_coraz%C3%B3n.svg/169px-Heart_coraz%C3%B3n.svg.png');
+		heart.setAttribute('src', './heart.png');
 		heart.setAttribute('class', 'heart');
 		button.appendChild(heart);
 		photo.appendChild(button);
@@ -56,9 +54,33 @@ $(document).ready(function() {
 			filter.innerHTML = filters[f];
 			filtersList.appendChild(filter);
 
-			rightDiv.innerHTML = rightDiv.innerHTML + filters[f] +", "
+			//rightDiv.innerHTML = rightDiv.innerHTML + filters[f] +", "
+			
 		}
 		pic.appendChild(filtersList);
+
+		var titleHolder = document.createElement('div');
+		var title = document.createElement('h4');
+		title.setAttribute('class','title');
+		title.innerHTML = "Stata Center";
+		var address = document.createElement('div');
+		address.innerHTML = "32 Vassar St, Cambridge, MA 02139";
+
+		var description = document.createElement('div');
+		description.innerHTML = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque non sapien vel odio tempus cursus. Donec vitae libero massa. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Maecenas elementum nisl id ullamcorper scelerisque. Nam quis arcu gravida, vestibulum ipsum ut, malesuada tellus. Morbi eu imperdiet elit. Morbi condimentum dignissim nunc scelerisque cursus. Mauris nec neque facilisis, consequat est quis, eleifend magna."
+
+		var pin = document.createElement('input');
+		pin.setAttribute('type', 'image');
+		pin.setAttribute('src', './pin.png');
+		pin.setAttribute('class', 'pinimage');
+		pin.style.filter = 'contrast(0%)'
+		title.appendChild(pin);
+
+
+		rightDiv.appendChild(titleHolder);
+		titleHolder.appendChild(title)
+		rightDiv.appendChild(address);
+		rightDiv.appendChild(description);
 
 		leftDiv.appendChild(photo);
 
@@ -69,7 +91,7 @@ $(document).ready(function() {
 	var hearts = document.getElementsByClassName('heart');
 	for (h=0; h<hearts.length; h++) {
 		var heart = hearts[h];
-		heart.style.filter = 'contrast(0%)';
+		heart.style.filter = 'grayscale(1.0)';
 	}
 
 });
@@ -79,9 +101,21 @@ $(document).on('click', '#pinned .heart', function(evt)
 	var photo = evt.target.parentElement.parentElement.cloneNode(true);
 	var heart = photo.childNodes[0].childNodes[0];
 
-	if (evt.target.style.filter == 'contrast(0%)') {
-		evt.target.style.filter = 'contrast(100%)';
+	if (evt.target.style.filter == 'grayscale(0)') {
+		evt.target.style.filter = 'grayscale(1.0)';
 	} else {
+		evt.target.style.filter = 'grayscale(0)';
+	}
+});
+
+$(document).on('click', '.pinimage', function(evt)
+{
+	var photo = evt.target.parentElement.parentElement.cloneNode(true);
+	var heart = photo.childNodes[0].childNodes[0];
+
+	if (evt.target.style.filter == 'contrast(100%)') {
 		evt.target.style.filter = 'contrast(0%)';
+	} else {
+		evt.target.style.filter = 'contrast(100%)';
 	}
 });
