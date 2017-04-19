6cd25cdcd2a277beea34a98c4f60d391fc0233d6
Wilbur <wbz@mit.edu>
2017-04-19 13:26:11 -0400

    portfolio

---
+++ script-portfolio.js
@@ -0,0 +1,73 @@
+var FILTERS = ['grass', 'water', 'building', 'sculpture'];
+var NUM_ALBUMS = 4;
+
+//loads albums and the new album buttons
+function loadAlbums(){
+    for (i=0; i<NUM_ALBUMS; i++) {
+        var name = "album " + i;
+
+        var album = document.createElement('button');
+        album.setAttribute('id', 'album-'+i);
+        album.setAttribute('class', 'album');
+
+        var albumName = document.createElement('div');
+        albumName.setAttribute('class', 'album-text');
+        albumName.setAttribute('display', 'block');
+        albumName.setAttribute('text-align', 'center');
+        albumName.setAttribute('vertical-align', 'center');
+        albumName.setAttribute('margin', 'auto');
+
+        albumName.innerHTML = name;
+        album.appendChild(albumName);
+
+        if (i%2 == 0) {
+            var column = document.getElementById('albums-col-1');
+            column.appendChild(album);
+        } else {
+            var column = document.getElementById('albums-col-2');
+            column.appendChild(album);
+        }
+    }
+
+    //for adding the new album button
+    var addNewAlbumName = "Add new album";
+    var newAlbum = document.createElement('button');
+    newAlbum.setAttribute('id', 'newAlbum');
+    newAlbum.setAttribute('class', 'album');
+
+    var newAlbumName = document.createElement('div');
+    newAlbumName.setAttribute('class', 'album-text');
+    newAlbumName.setAttribute('display', 'block');
+    newAlbumName.setAttribute('text-align', 'center');
+    newAlbumName.setAttribute('vertical-align', 'center');
+    newAlbumName.setAttribute('margin', 'auto');
+
+    newAlbumName.innerHTML = addNewAlbumName;
+    newAlbum.appendChild(newAlbumName);
+
+    if (NUM_ALBUMS%2 == 0){
+        var column = document.getElementById('albums-col-1');
+        column.appendChild(newAlbum);
+    } else {
+        var column = document.getElementById('albums-col-2');
+        column.appendChild(newAlbum);
+    }
+}
+
+//for loading a particular album
+function loadAlbum(albumName){
+
+}
+
+//for loading the new album page
+function loadNewAlbum(){
+    
+}
+
+$(document).ready(function() {
+    loadAlbums();
+});
+
+$(document).on('click', '#newAlbum', function(evt) {
+    loadNewAlbum();
+});
