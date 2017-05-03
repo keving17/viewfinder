cbe04c35c157cb1fdd6f9c0a132a9c2a4973d288
Kimberly Yu <kimbeeyu@mit.edu>
2017-05-03 16:09:41 -0400

    Edited heart and favorites formatting

--- script.js
+++ script.js
@@ -1,19 +1,11 @@
 // var FILTERS = ['grass', 'water', 'building', 'sculpture'];
 var SOURCES = ['alchemist', 'dome', 'dome2', 'kresge', 'redsculpture', 'river', 'simmons', 'stata', 'stata2', 'stata3'];
 var NUM_PHOTOS = 8
+var row_num = 1;
 
 $(document).ready(function() {
 	var available = SOURCES.slice();
 	for (p=0; p<NUM_PHOTOS; p++) {
-		// var available = FILTERS.slice();
-		// var filters = [];
-		// var num_filters = Math.floor(Math.random()*FILTERS.length)+1;
-		// for (r=0; r<num_filters; r++) {
-		// 	var random = Math.floor(Math.random()*available.length);
-		// 	var f = available[random];
-		// 	available.splice(random,1);
-		// 	filters.push(f);
-		// }
 		var random = Math.floor(Math.random()*available.length);
 		var src = './images/'+available[random]+'.jpg';
 		available.splice(random,1);
@@ -30,41 +22,13 @@ $(document).ready(function() {
 
 		var heart = document.createElement('input');
 		heart.setAttribute('type', 'image');
-		heart.setAttribute('src', './images/heart.png');
-		heart.setAttribute('class', 'heart');
+		heart.setAttribute('src', './images/whiteheart.png');
+		heart.setAttribute('height', '20px');
+		heart.setAttribute('class', 'heart unheart');
 		photo.appendChild(heart);
 
-		// var pic = document.createElement('div');
-		// pic.setAttribute('id', 'inspr-'+p);
-		// pic.setAttribute('class', 'pic');
-		// photo.appendChild(pic);
-
-		// var filtersList = document.createElement('div');
-		// filtersList.setAttribute('class', 'filters');
-		// filtersList.setAttribute('display', 'block');
-		// filtersList.setAttribute('text-align', 'center');
-		// filtersList.setAttribute('vertical-align', 'center');
-		// filtersList.setAttribute('margin', 'auto');
-		// for (f=0; f<filters.length; f++) {
-		// 	var filter = document.createElement('div');
-		// 	filter.innerHTML = filters[f];
-		// 	filtersList.appendChild(filter);
-		// }
-		// pic.appendChild(filtersList);
-
-		if (p%2 == 1) {
-			var column = document.getElementById('inspr-col-1');
-			column.appendChild(photo);
-		} else {
-			var column = document.getElementById('inspr-col-2');
-			column.appendChild(photo);
-		}
-	}
-
-	var hearts = document.getElementsByClassName('heart');
-	for (h=0; h<hearts.length; h++) {
-		var heart = hearts[h];
-		heart.style.filter = 'grayscale(0.75)';
+		var row = document.getElementById('inspiration');
+		row.appendChild(photo);
 	}
 });
 
@@ -72,32 +36,23 @@ $(document).on('click', '#inspiration .heart', function(evt)
 {
 	var photo = evt.target.parentElement.cloneNode(true);
 	var heart = evt.target;
-
-	if (heart.style.filter == 'grayscale(0.75)') {
-	// if (heart.src == './images/unheart.png') {
-		heart.style.filter = 'grayscale(0)';
-		photo.childNodes[1].style.filter = 'grayscale(0)';
-		// heart.setAttribuet('src', './images/heart.png');
-		// var column;
-		 var column1 = document.getElementById('fav-col-1');
-		 var column2 = document.getElementById('fav-col-2');
-		var row = document.getElementById('favorites');
-
-		 if (column1.childNodes.length == column2.childNodes.length) {
-		 	column = column1;
-		 } else {
-		 	column = column2;
-		 }
-
+	if (heart.className == 'heart unheart') {
+		heart.setAttribute('src', './images/heart.png');
+		heart.className = 'heart faved';
+		
+		var row = document.getElementById('favorites')
+		photo = evt.target.parentElement.cloneNode(true);
 		photo.setAttribute('id', 'fav'+photo.id.substr(5,photo.id.length));
-
-		 column.appendChild(photo);
-		//row.appendChild(photo);
+		row.appendChild(photo);
+		
 	} else {
-		heart.style.filter = 'grayscale(0.75)';
-		// heart.setAttribute('src', './images/unheart.png');
+		heart.setAttribute('src', './images/whiteheart.png');
+		heart.className = 'heart unheart';
+
 		var fav = document.getElementById('fav'+photo.id.substr(5,photo.id.length));
-		fav.parentElement.removeChild(fav);
+		$('#'+fav.id).fadeOut(500,function(){
+  			this.remove();
+  		});
 	}
 });
 
@@ -106,13 +61,12 @@ $(document).on('click', '#favorites .heart', function(evt)
 	var photo = evt.target.parentElement;
 	var heart = evt.target;
 	var inspr = document.getElementById('photo-'+photo.id.substr(4,photo.id.length));
-	console.log('photo'+photo.id.substr(4,photo.id.length));
-	inspr.childNodes[1].style.filter = 'grayscale(0.75)';
-	heart.style.filter = 'grayscale(0.75)';
-	// inspr.childNodes[0].childNodes[0].src = './images/unheart.png';
-	// heart.src = './images/unheart.png';
 
-	// photo.parentElement.removeChild(photo);
+	inspr.childNodes[1].src = './images/whiteheart.png';
+	inspr.childNodes[1].className = 'heart unheart';
+	heart.src = './images/whiteheart.png';
+	heart.className = 'heart unheart';
+
 	$('#'+photo.id).fadeOut(500,function(){
   			this.remove();
   	});
