8a047e9909e345f2a80d31212582ccc5b94601ed
Ryan Pijai <ryan@pijai.com>
2017-05-08 13:15:55 -0400

    change title of direction page

--- script-directions.js
+++ script-directions.js
@@ -4,6 +4,7 @@ $(document).ready(function() {
 	var URL = decodeURIComponent(window.location.href);
 	var index = URL.indexOf('=');
 	var placeName = URL.substr(index+1,URL.length);
+  document.title = placeName;
 	document.getElementById('placeName').innerHTML = placeName;
 
   var random = Math.floor(Math.random()*NUM_MAPS) + 1;
